5116ae136d75f2b6544e42bd653c19477fa3260c
benalak11 <mm8584856@gmail..com>
2025-07-11 00:54:14 +0100

    task 2

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2);
+
+
 if (args.length === 0) {
     console.log("No argument");
-}
-else if (args.length === 1) {
+} else if (args.length === 1) {
     console.log("Argument found");
-}
-else {
+} else {
     console.log("Argument found");
 }
