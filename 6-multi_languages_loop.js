6e763e4a52ad096e7960a144ff583b9be672ed40
benalak11 <mm8584856@gmail..com>
2025-07-14 23:58:23 +0100

    task 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,1 +1,9 @@
-console.log($[C is fun $[Python is cool] $[JavaScript is amazing]);
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const msg of messages) {
+  console.log(msg);
+}
